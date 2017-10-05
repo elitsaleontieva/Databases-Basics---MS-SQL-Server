172875ee29e4dafa247dbf067478d01a2456575c
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-10-05 16:16:43 +0300

    Create 04. Self-Referencing.sql

---
+++ Table Relations/04. Self-Referencing.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Teachers(
+TeacherID INT NOT NULL PRIMARY KEY,
+Name NVARCHAR(50) NOT NULL,
+ManagerID INT
+)
+ALTER TABLE Teachers
+ADD CONSTRAINT FK FOREIGN KEY (ManagerID) REFERENCES Teachers(TeacherID);
+
+INSERT INTO Teachers
+VALUES
+(101,'John'	,NULL),
+(102,'Maya'	,106),
+(103,'Silvia',106),
+(104,'Ted' ,105),
+(105,'Mark'	,101),
+(106,'Greta',101)
