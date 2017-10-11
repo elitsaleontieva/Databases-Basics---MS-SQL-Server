6cdbdbaee48e05bfd25f779039ad20250e1095f2
Elitsa Leontieva <ellyleontieva@abv.bg>
2017-10-12 00:12:01 +0300

    Create 06. Employees Hired After.sql

---
+++ Joins- Subqueries- CTE and Indices/06. Employees Hired After.sql
@@ -0,0 +1,10 @@
+SELECT
+e.FirstName,
+e.LastName ,
+e.HireDate ,
+d.Name AS DeptName
+FROM Employees AS e
+  LEFT OUTER JOIN Departments AS d
+  ON e.DepartmentID = d.DepartmentID
+ WHERE e.HireDate > '1999-1-1' AND d.Name IN('Sales','Finance')
+ ORDER BY e.HireDate
